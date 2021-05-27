2ddbb4f2352df7b38a4397efb647599d6e7c3d79
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-05-27 22:48:44 +0300

    10

---
+++ 2021-19-05/task10/app.js
@@ -0,0 +1,17 @@
+//Задание 10
+//Найдите и исправьте ошибки в коде:
+
+var x = prompt('x', 0);
+var error;
+
+switch (String(x)) {
+    case '0': error = 'На ноль делить нельзя';break;
+    case '1': error = 'На единицу делить бессмысленно';break;
+    case 'NaN': error = 'Не математическая операция';break;
+}
+
+if (error) {
+    alert(error);
+} else {
+    alert(100 / x);
+}
