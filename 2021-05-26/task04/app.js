4d545e989f2060d56bc84f948b174c29bf3879da
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-02 14:50:16 +0300

    04

---
+++ 2021-05-26/task04/app.js
@@ -0,0 +1,22 @@
+// LEVEL UP задание 4
+//Используя вложенные циклы, сформируйте строку чередованием символов . и # таким образом, чтобы получилась шахматный узор. Для перевода строки используйте символ \n. Код должен поддерживать легкое изменение размеров доски.
+
+// var x = +prompt('Enter quantity of lines', 10);
+// var y = +prompt('Enter quantity of symbol in lines', 10);
+// var c = '#', d = '.';
+// var string = '.';
+ 
+// for(let i = 1; i < x; i++){
+//     for(let j = 1; j < y ; j++){
+//        if(j % 2 == 0){
+//            string = string + d;
+//        }
+//        else{
+//         string = string + c;
+//        }
+    
+//     }
+//     console.log(string + '\n');
+//     string = '.';
+// }
+
