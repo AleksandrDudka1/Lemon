e5ed1984482d2fa2e557bd243b464c2a3cba803c
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-25 12:19:19 +0300

    03

---
+++ 2021-06-04/task03/app.js
@@ -0,0 +1,16 @@
+    // Задание 3
+    //Используя синтаксис ES5, напишите функцию-аналог Math.min(). Функция принимает любое количество чисел и возвращает меньшее из них:
+
+// function min(str){
+//         let mas = str.split(' ');
+//         mas = mas.map(Number);
+//         let min = mas[0];
+//         for (let i = 0; i < mas.length; i++) {
+//           if(mas[i] < min) {
+//             min = mas[i];
+//           }
+//         }
+//         console.log(mas);
+//         return min;
+//   };
+//   console.log(min('2 2 34 5 21 35 6 7 6543  212 -43432 32 323'));
