8eeae28758cc7cab40f2a484ebcb033f4a7ef27a
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-25 12:25:28 +0300

    05

---
+++ 2021-06-04/app.js
@@ -0,0 +1,21 @@
+    // Задание 5
+    //Напишите функцию, которая в строке, состоящей из чисел, разделенных пробелом, находит максимальное и минимальное и возвращает их 
+
+  
+
+
+    function number(result) {
+      result = result.split(' ');
+      result = result.map(Number);
+      min = 0;
+      max = min;
+      for(let i = 0; i < result.length; i++) {
+        if(result[i] < min) min = result[i];
+        if(result[i] > max) max = result[i];
+      }
+      console.log(`Минимальное значение: ${min}, максимальное значение: ${max}`)
+      return result;
+
+    }
+    var res = number('4 5 29 54 4 0 -214 542 -64 1 -3 6 -6');
+    console.log(res);
