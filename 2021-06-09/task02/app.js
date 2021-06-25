7e24950fa6e11bab3940517acedeb5c412694864
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-25 12:42:29 +0300

    02

---
+++ 2021-06-09/task02/app.js
@@ -0,0 +1,23 @@
+ // Задание 2
+    //Напишите функцию checkNumber, которая получает на вход как параметр массив любых значений и возвращает true, если все элементы - числа, и false - если в массиве хотя бы  одно не число. Для проверки массива используйте метод every или some
+
+  //   function checker( mas ){
+
+  //     const num = (currentValue) =>{ 
+  //         if(!isNaN(currentValue)){
+  //         return true;
+  //     }else {
+  //         return false;
+  //     }};
+  
+  //     if(mas.every(num)){
+  //         return true;
+  //     }
+  //     else {
+  //         return false;
+  //     };
+
+  // }
+  
+  // var mas = [1, 30, 39, 29, 13, '34x'];
+  // console.log(checker(mas));
