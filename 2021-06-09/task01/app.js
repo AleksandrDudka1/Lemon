310226f5eb675dec5e7d2417f2a3ed7332ff8972
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-25 12:39:48 +0300

    01

---
+++ 2021-06-09/task01/app.js
@@ -0,0 +1,15 @@
+    // Задание 1
+    //Найдите и исправьте ошибки в коде:
+
+    // function foo() {
+    //   function bar(a) {
+    //     i = 3;
+    //     return i + a;
+    //   }
+      
+    //   for (let i = 0; i < 10; i++) {
+    //     console.log( bar(i * 2) );
+    //   }
+    // }
+    
+    // foo();
