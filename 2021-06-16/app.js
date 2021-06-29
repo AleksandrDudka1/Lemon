f9dbda01591fa23a1e42924192b3885d00915885
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-29 12:36:39 +0300

    01

---
+++ 2021-06-16/app.js
@@ -0,0 +1,46 @@
+
+  /////2021-06-16
+
+
+//  document.querySelector('body').addEventListener('click', clickHandler);
+ 
+// function clickHandler(event) {
+            
+//         if(!event.target.hasAttribute('data-x')) return;
+        
+//         let { target: tableElement } = event;
+//         console.log(tableElement);
+         
+          
+
+                    
+//         if(!tableElement.classList.contains('black'))  {
+//           tableElement.classList.add('black');
+//           tableElement.setAttribute('data-x', '1')
+//         }else {
+//           tableElement.setAttribute('data-x', '')
+//           tableElement.classList.remove('black');
+
+//         }
+
+        
+//         document.querySelector('.clear-btn').addEventListener('click', clickBtn);
+
+//         function clickBtn(event) {
+//          if(tableElement.getAttribute('data-x', '1')) {
+          
+//           tableElement.classList.remove('black');
+//           }
+//         }
+//         document.querySelector('.remove-btn').addEventListener('click', clickBtns);
+//         function clickBtns() {
+//              if (tableElement.getAttribute('data-x', 1)) {
+//                return tableElement.classList.add('black');;
+//              }
+//         }     
+//         }
+
+  
+
+
+
