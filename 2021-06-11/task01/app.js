d2b37f1cfe08f012de80ab6bd0942b172fc31f97
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-29 12:25:10 +0300

    01

---
+++ 2021-06-11/task01/app.js
@@ -0,0 +1,26 @@
+  //2021-06-11
+  // Задание 1
+  //вариант 1
+  //Создайте объект coffeeMachine со свойством message: ‘Your coffee is ready!’ и методом start(), при вызове которого – coffeeMachine.start() – через 3 секунды появляется окно с сообщением, записанным в свойстве объекта message.
+
+//   var coffeeMachine = {
+//     message :" Your coffee is ready!",
+//     start : setTimeout(() => {
+//          alert(coffeeMachine.message)
+        
+//         }, 3000),
+ 
+//  }
+ 
+//  coffeeMachine.start();
+  //вариант 2
+  // var coffeeMachine = {
+  //   message:  `Your coffee is ready!`,
+  //   start: function(){
+  //        alert(coffeeMachine.message)
+  //   }
+  // }
+
+  //  setTimeout( coffeeMachine.start, 3000);
+
+
