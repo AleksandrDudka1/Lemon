8ba7104613712b04b908ea3b964c84838246efee
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-29 12:27:26 +0300

    02

---
+++ 2021-06-11/task02/app.js
@@ -0,0 +1,21 @@
+//Задание 2 
+  //Создайте функцию hello(), которая выводит приветствие пользователю. Создайте два объекта, содержащие поля firstname, lastname. Используя метод call и функцию hello() приветствуйте каждого из пользователей персонально.
+  
+  // function hello() {
+  //      alert(`Hello, ${this.firstName} ${this.lastName}`);
+  // }
+
+
+
+
+  // let person1 = {
+  //   firstName: `Sasha`,
+  //   lastName: `Dudka`,
+  // }
+  // let person2 = {
+  //   firstName: `Dima`,
+  //   lastName: `Dudka`,
+  // }
+
+  // hello.call(person1);
+  // hello.call(person2);
