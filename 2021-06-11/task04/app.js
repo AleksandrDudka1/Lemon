6d4551f0f2100e8f128b4104cea2d7987a77ae89
Александр Викторович Дудка <aleksandrviktorovicdudka@MBP-Aleksandr.lan>
2021-06-29 12:32:28 +0300

    04

---
+++ 2021-06-11/task04/app.js
@@ -0,0 +1,26 @@
+//Задание 4 
+
+//Придумайте алгоритм расчета суммы всех фактических параметров функции с использованием только рекурсии:
+//console.log( sum(1, 2, 3, 4, 5) ); // 15
+
+
+
+
+
+// function sum() {
+//   function add(arguments, i){
+//     if(i === arguments.length-1){
+//         return arguments[i];
+//     }
+  
+//     return arguments[i] + add(arguments, i+1);
+//   }
+// if(arguments.length === 0){
+//   return 0;
+// }
+// return add(arguments, 0);
+// };
+
+
+// console.log(  sum(1, 2, 3, 4, 5));
+
